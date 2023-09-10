8b4d7d41434796623a0c7a1f58b91fc77e4db833
Nishantjha786 <36167637+Nishantjha786@users.noreply.github.com>
2023-09-10 21:04:50 +0530

    getElementbyclassname

--- dom.js
+++ dom.js
@@ -3,5 +3,9 @@ headerTitle.style.color ="#999";
 var x = document.getElementById('xyz');
 x.style.color = "green";
 x.style.fontWeight = "bold";
-
-
+var y = document.querySelectorAll("li");
+y[2].style.backgroundColor = "green";
+for(let i=0 ; i<y.length;i++)
+{
+    y[i].style.fontWeight= "bold";
+}
