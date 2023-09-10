40c90a3e2828ee26703d3d119a3a2fd11a0a7c45
Nishantjha786 <36167637+Nishantjha786@users.noreply.github.com>
2023-09-10 20:01:42 +0530

    DOM assigment 1

---
+++ dom.js
@@ -0,0 +1,7 @@
+var headerTitle = document.getElementById('header-title');
+headerTitle.style.color ="#999";
+var x = document.getElementById('xyz');
+x.style.color = "green";
+x.style.fontWeight = "bold";
+
+
